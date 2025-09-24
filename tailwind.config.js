63c261a78c4603b3f035fad942cf841c68594850
hector1-cloud <hectorruiz9992@gmail.com>
2025-09-24 00:27:39 -0500

    Create tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        azulNoche: "#0B1D3A",
+        plateado: "#C0C7D1",
+        rojoProfundo: "#8B0000",
+        ambar: "#D4A373",
+        dorado: "#D4AF37"
+      }
+    },
+  },
+  plugins: [],
+};
